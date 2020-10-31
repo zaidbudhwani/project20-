7e887b110ec4f8e3c973aaa0d457a71d3a2fbc59
zaidbudhwani <71475749+zaidbudhwani@users.noreply.github.com>
2020-10-31 15:02:53 -0500

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# project20-
+# MatterJSBoilerPlate
+MatterJSBoilerPlate
---
+++ sketch.js
@@ -0,0 +1,41 @@
+var options={
+	isStatic:false,
+	restituion:0.3,
+frition:0.5,
+density:1.2
+}
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	
+}
+
+function setup() {
+	createCanvas(800, 700);
+
+
+	engine = Engine.create();
+	world = engine.world;
+
+	//Create the Bodies Here.
+
+
+	Engine.run(engine);
+  
+}
+
+
+function draw() {
+  rectMode(CENTER);
+  background(0);
+  
+  drawSprites();
+ 
+}
+
+
+
